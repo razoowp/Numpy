18183a3bcadef53216787a332875864d7197edd2
razoowp <razoowp@gmail.com>
2017-07-06 11:22:39 +0545

    Creating vector and matrix numpy array

---
+++ vector-matrix-creation.py
@@ -0,0 +1,14 @@
+# A 1-dimensional array is refferred to as Vector and 2-dimensional array is
+# reffered to as matrix
+
+#Question/Instructions
+'''
+    - Create a vector from the list [10, 20, 30].
+        Assign the result to the variable vector.
+    - Create a matrix from the list of lists [[5, 10, 15], [20, 25, 30], [35, 40, 45]].
+        Assign the result to the variable matrix.
+
+'''
+import numpy as np
+vector = np.array([10, 20, 30])
+matrix = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
