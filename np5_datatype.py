225c68fd20badfd5a03caae65f1a0b7562fddc1b
razoowp <razoowp@gmail.com>
2017-07-19 10:52:03 +0545

    Numpy, finding data type

---
+++ np5_datatype.py
@@ -0,0 +1,12 @@
+'''
+Assign the data type of world_alcohol to the variable world_alcohol_dtype.
+Display world_alcohol_dtype using the print() function.
+'''
+import numpy as np
+world_alcohol = np.array([1,2,3,4])
+world_alcohol_dtype = world_alcohol.dtype
+print(world_alcohol_dtype)
+
+'''
+output: int64
+'''
